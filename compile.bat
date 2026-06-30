de93e451fabe4a5995216a1233eb82dfb67e2ef0
Nithin E <nithinx002@gmail.com>
2026-07-01 01:34:08 +0530

    new script is added

---
+++ compile.bat
@@ -0,0 +1,13 @@
+@echo off
+echo =========================================
+echo       Compiling DA_Resume.tex...
+echo =========================================
+echo.
+
+pdflatex DA_Resume.tex
+
+echo.
+echo =========================================
+echo   Compilation Finished! Check the PDF.
+echo =========================================
+pause
